16dd86b1e9b781190b64c8e9d09a6d054dff64a9
NTuanHung123 <tuanhung231006@gmail.com>
2026-04-21 11:29:05 +0700

    Add files via upload

---
+++ src/bfs_solver.py
@@ -0,0 +1,27 @@
+from collections import deque
+from src.core_logic import get_start_goal, get_neighbors, print_maze_with_path
+
+def bfs(maze):
+    start, goal = get_start_goal(maze)
+    queue = deque([(start, [start])])
+    visited = set([start])
+    
+    visited_count = 0
+    
+    while queue:
+        current, path = queue.popleft()
+        visited_count += 1
+        
+        # Nếu đến đích
+        if current == goal:
+            print_maze_with_path(maze, path, visited_count)
+            return path
+        
+        # Duyệt các ô kề
+        for neighbor in get_neighbors(maze, current):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append((neighbor, path + [neighbor]))
+    
+    print("Không tìm thấy đường đi!")
+    return None
